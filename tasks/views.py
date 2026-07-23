9c4ce603fa13c24ec3b63a36ff4027b8bbca515b
RealRick37 <122051997+RealRick37@users.noreply.github.com>
2026-07-23 13:43:33 +0330

    Add filtering, search ordering and pagination to tasks API

--- tasks/views.py
+++ tasks/views.py
@@ -1,11 +1,18 @@
 from rest_framework import generics
 from .models import Task
 from .serializers import TaskSerializer
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
 
 # Create your views here.
 
 class TaskListCreateView(generics.ListCreateAPIView):
     serializer_class=TaskSerializer
+    filter_backends=[DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_fields=["status"]
+    search_fields=["title", "description"]
+    ordering_fields=["created_at", "updated_at", "daedline"]
+    ordering=["-created_at"]
 
     def get_queryset(self):
         return Task.objects.filter(user=self.request.user)
